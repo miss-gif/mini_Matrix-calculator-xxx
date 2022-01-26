f54e774ac71adadf7ba943de259efbede2a0a463
miss-gif <ddc.akari@gmail.com>
2022-01-27 02:39:54 +0900

    add

---
+++ main.js
@@ -0,0 +1,8 @@
+let produce_btn = document.querySelector("#produce_btn"); //생성하기 버튼
+produce_btn.addEventListener("click", function () {
+  let procession_A_row = document.querySelector("#procession_A_row").value;
+  let procession_A_column = document.querySelector("#procession_A_column").value;
+  let procession_B_row = document.querySelector("#procession_B_row").value;
+  let procession_B_column = document.querySelector("#procession_B_column").value;
+  alert("3");
+});
